dcbc260cd5061e1d99950ffb0185472ff54cd119
vishnath <vishnusnathan@gmail.com>
2020-08-25 14:26:57 +0530

    first commit

--- src/Advice.js
+++ src/Advice.js
@@ -1,16 +1,16 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import './Advice.css'
 
 function Advice() {
 
-const [advice,setAdvice]=useState("")
-const getAdvice=()=>{
-    axios.get('https://api.adviceslip.com/advice')
+const [advice,setAdvice]=useState("");
+const getAdvice=async ()=>{
+
+    await axios.get('https://api.adviceslip.com/advice')
     .then(function (response) {
       // handle success
-      console.log(response.data.slip.advice);
-      setAdvice(response.data.slip.advice)
+      setAdvice(response.data.slip.advice);
     })
     .catch(function (error) {
       // handle error
@@ -21,10 +21,22 @@ const getAdvice=()=>{
     });
 }
 
+useEffect(() => {
+  getAdvice();
+}, [])
+
     return (
         <div className="advice">
-            <p>{advice}</p>
-            <button onClick={()=>{getAdvice()}}>Get Advice</button>
+            <p>
+              {advice == ""?
+                "Loading..."
+                :
+                advice
+              }
+            </p>
+            <button onClick={()=>{getAdvice()}} 
+             disabled = {advice == "" ? true : false}
+            >Get Advice</button>
         </div>
     )
 }
